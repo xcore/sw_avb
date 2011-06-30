a7aeb3d67eb3f1bbffe3fc8d5cdae8999bf4165e
David Norman <davidn@xmos.com>
2011-06-30 10:28:34 -0700

    Simple source code rearrangement.  personally i don't like named structures typedef'd to a type with the same name.

--- module_avb_srp/src/avb_mrp_pdu.h
+++ module_avb_srp/src/avb_mrp_pdu.h
@@ -1,33 +1,33 @@
 #ifndef __avb_mrp_pdu_h__
 #define __avb_mrp_pdu_h__
 
-typedef struct mrp_ethernet_hdr {
+typedef struct {
   unsigned char dest_addr[6];
   unsigned char src_addr[6];
   unsigned char ethertype[2];
 } mrp_ethernet_hdr;
 
 
-typedef struct mrp_header {
+typedef struct {
   unsigned char ProtocolVersion;
 } mrp_header;
 
-typedef struct mrp_msg_header {
+typedef struct {
   unsigned char AttributeType;
   unsigned char AttributeLength;
   unsigned char AttributeListLength[2];
 } mrp_msg_header;
 
-typedef struct mrp_vector_header {
+typedef struct {
   unsigned char LeaveAllEventNumberOfValuesHigh;
   unsigned char NumberOfValuesLow;
 } mrp_vector_header;
 
-typedef struct mrp_footer {
+typedef struct {
   unsigned char EndMark[2];
 } mrp_footer; 
 
-typedef struct mrp_msg_footer {
+typedef struct {
   unsigned char EndMark[2];
 } mrp_msg_footer; 
 
