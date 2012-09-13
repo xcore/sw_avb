06f0f5e786f9c98ed8b3f02d4cc0fbde531f59ba
Andy Lucas <andy@xmos.com>
2012-09-13 18:41:57 +0100

    MAAP should redo SRP reservation if MAAP fails

--- module_avb_1722_maap/src/avb_1722_maap_app_hooks.c
+++ module_avb_1722_maap/src/avb_1722_maap_app_hooks.c
@@ -26,6 +26,10 @@ void __attribute__((weak)) avb_talker_on_source_address_reserved(int source_num,
 #if AVB_1722_1_TALKER_ENABLED
   avb_1722_1_talker_set_mac_address(source_num, mac_addr);
 #else
+  if (state > AVB_SOURCE_STATE_DISABLED)
+  {
+    set_avb_source_state(source_num, AVB_SOURCE_STATE_DISABLED);
+  }
   set_avb_source_state(source_num, AVB_SOURCE_STATE_POTENTIAL);
 #endif
 }
