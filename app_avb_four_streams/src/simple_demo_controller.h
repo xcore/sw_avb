9ae5963c9bb0d530117d3a89f022bcaa258ca3c2
Andy Lucas <andy@xmos.com>
2013-03-12 17:32:12 +0000

    Adding 4 streams example app

---
+++ app_avb_four_streams/src/simple_demo_controller.h
@@ -0,0 +1,6 @@
+#ifndef _SIMPLE_DEMO_CONTROLLER_h_
+#define _SIMPLE_DEMO_CONTROLLER_h_
+
+void simple_demo_controller(REFERENCE_PARAM(int, change_stream), REFERENCE_PARAM(int, enable_remote), chanend c_tx);
+
+#endif
