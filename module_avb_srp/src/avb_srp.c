e0b8b71ee4da038dc90e72ce73dbb4dd22da8eb3
Andy Lucas <andy@xmos.com>
2013-10-08 15:05:03 +0100

    Decrease shaper credit when Talker is local

--- module_avb_srp/src/avb_srp.c
+++ module_avb_srp/src/avb_srp.c
@@ -416,6 +416,10 @@ void avb_srp_listener_leave_ind(CLIENT_INTERFACE(avb_interface, avb), mrp_attrib
   	if (state == AVB_SOURCE_STATE_ENABLED && !matched_listener_opposite_port) {
   #if SRP_AUTO_TALKER_STREAM_CONTROL
   		avb_set_source_state(avb, stream, AVB_SOURCE_STATE_POTENTIAL);
+      if (stream_table[entry].bw_reserved[attr->port_num] == 1) {
+        srp_decrease_port_bandwidth(sink_info->reservation.tspec_max_frame_size, 0, attr->port_num);
+        stream_table[entry].bw_reserved[attr->port_num] = 0;
+      }
   #else
   #endif
 	 }
